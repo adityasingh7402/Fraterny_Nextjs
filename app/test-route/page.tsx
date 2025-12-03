022def5cdc79017045bf5ed32601b8c376c34d97
Indranil Maiti <indranilmaiti1@gmail.com>
2025-12-03 12:00:40 +0100

    feat/results-new-design

---
+++ app/test-route/page.tsx
@@ -0,0 +1,14 @@
+import TestPage from "./components/TestPage"
+
+
+
+
+export default function page() {
+    return (
+        <div>
+            <div>
+                <TestPage />
+            </div>
+        </div>
+    );
+}
