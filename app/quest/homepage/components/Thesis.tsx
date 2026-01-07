18ef5c3545f80959e5205009efca0ab30c940f8e
Indranil Maiti <indranilmaiti1@gmail.com>
2026-01-07 11:30:02 +0100

    feat/quest-design-homepage-add

--- app/quest/homepage/components/Thesis.tsx
+++ app/quest/homepage/components/Thesis.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export const Thesis: React.FC = () => {
   return (
+    <div>
     <section className="bg-black text-white py-20 px-6 black-bg-section">
       <div className="max-w-3xl mx-auto">
         <h3 className="text-gray-400 font-gilroy-semibold mb-6 uppercase tracking-wider text-xl">The <span className=" text-xl">Quest</span> Mode</h3>
@@ -56,5 +58,66 @@ export const Thesis: React.FC = () => {
         </div>
       </div>
     </section>
+    <div className='container mx-auto max-w-7xl flex flex-col sm:flex-row items-center justify-between gap-8 sm:gap-24 pt-8'>
+        <div className='relative w-80 h-80 md:w-[600px] md:h-[800px]'>
+          <Image
+            src='/quest/quest-pdf-page-1.webp'
+            alt='Thesis Illustration'
+            layout='fill'
+            objectFit='contain'
+            priority
+          />
+        </div>
+        <div className='p-4 sm:p-0 relative '>
+          {[
+            {
+              id: "extract-1",
+              title: "Reveals Your True Self",
+              description: "Quest helps you understand your core motivations, strengths, and weaknesses, providing a clear picture of your current self."
+            },
+            {
+              id: "extract-2",
+              title: "Identifies the Gap",
+              description: "It highlights the gap between your current reality and your ideal self, showing you exactly what's holding you back."
+            },
+            {
+              id: "extract-3",
+              title: "Provides Personalized Tools",
+              description: "Based on your unique persona, Quest delivers hyper-personalized psychological frameworks and tools to take immediate action."
+            }
+          ].map((item) => (
+            <li className="group pb-4 mb-4 border-b border-gray-300 list-none" key={item.id}>
+              <input
+                type="radio"
+                name="accordion"
+                id={item.id}
+                className="peer hidden"
+                defaultChecked={item.id === "extract-1"}
+              />
+              <label
+                htmlFor={item.id}
+                className="transition-colors peer-checked:text-black text-neutral-400 cursor-pointer block"
+              >
+                <p className="text-3xl sm:text-5xl font-gilroy-semibold">{item.title}</p>
+                <div className="grid grid-rows-[0fr] transition-[grid-template-rows] duration-300 ease-in-out group-has-checked:grid-rows-[1fr]">
+                  <div className="overflow-hidden text-lg md:text-xl font-gilroy-regular">
+                    {item.description}
+                  </div>
+                </div>
+              </label>
+            </li>
+          ))}
+
+          <div className='pt-20'>
+            <Link href='/quest/quest-mode'>
+              <div className="text-2xl md:text-3xl font-bold text-neutral-900 sm:text-neutral-400 hover:text-neutral-900  transition-colors duration-300 flex flex-row items-center gap-4 cursor-pointer underline">
+                Enter Quest Mode.
+                <ArrowRight />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
